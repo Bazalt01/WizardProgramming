020f1a91b2b07c9f72594e933a28aae3186e9084
Глеб Токмаков <gleb.tokmakov@onetwotrip.com>
2016-11-26 18:52:05 +0300

    Initial Commit

---
+++ Wizard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Wizard
+//
+//  Created by Глеб Токмаков on 26.11.16.
+//  Copyright © 2016 Master. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
